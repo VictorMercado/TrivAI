11aa7c6e20e7b2e8f8ff620c7d0aadb8067d285c
Victor Mercado <victormercado@outlook.com>
2024-02-07 17:29:42 -0800

    remove unnecessary concatenated host

--- apps/web/src/store.ts
+++ apps/web/src/store.ts
@@ -28,7 +28,7 @@ const initialState: UserState = {
 export const useStore = create<UserState & Actions>((set, get) => ({
     ...initialState,
     updateUserName: async (name) => {
-        const response = await fetch(new URL("/api/updateName", checkEnvironment()), {
+        const response = await fetch(new URL("/api/updateName"), {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
@@ -42,7 +42,7 @@ export const useStore = create<UserState & Actions>((set, get) => ({
         set({name: nameResponse});
     },
     incrementScore: async (pointAmount) => {
-        const response = await fetch(new URL("/api/score", checkEnvironment()), {
+        const response = await fetch(new URL("/api/score"), {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
@@ -56,7 +56,7 @@ export const useStore = create<UserState & Actions>((set, get) => ({
         set({totalScore: score});
     },
     removeCheat: async () => {
-        const response = await fetch(new URL("/api/cheat", checkEnvironment()), {
+        const response = await fetch(new URL("/api/cheat"), {
             method: 'PUT',
             headers: {
                 'Content-Type': 'application/json'
@@ -71,7 +71,7 @@ export const useStore = create<UserState & Actions>((set, get) => ({
     resetScore: async () => {
         let response; 
         try {
-            response = await fetch(new URL("/api/resetScore", checkEnvironment()), {
+            response = await fetch(new URL("/api/resetScore"), {
                 method: 'PUT',
                 headers: {
                     'Content-Type': 'application/json'
@@ -91,7 +91,7 @@ export const useStore = create<UserState & Actions>((set, get) => ({
     deleteAccount: async () => {
         let response; 
         try {
-            response = await fetch(new URL(`/api/delete/${get().id}`, checkEnvironment()), {
+            response = await fetch(new URL(`/api/delete/${get().id}`), {
                 method: 'DELETE',
             });
             if (!response.ok) {
